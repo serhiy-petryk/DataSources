f5f455e829abb514168ab098810d0cfa7ed21e0e
serhiy-petryk <sergei_petrik@yahoo.com>
2023-07-02 20:06:42 +0300

    x

--- DataSources/Tests_Data/DataSources/Tests.cs
+++ DataSources/Tests_Data/DataSources/Tests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using DSCore.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
