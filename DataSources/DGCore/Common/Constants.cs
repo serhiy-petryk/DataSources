308e1a7cb8d620a4bea7d339033d382467b91536
serhiy-petryk <sergei_petrik@yahoo.com>
2023-07-10 21:59:00 +0300

    rollback previous commit

--- DataSources/DGCore/Common/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DGCore.Common
-{
-    public static class Constants
-    {
-        public const string MDelimiter = "^"; // Member delimiter for nested properties
-        public const string GroupItemCountColumnName = "#GroupItemCount";
-        public const string GroupColumnNamePrefix = "#Group_";
-    }
-}
